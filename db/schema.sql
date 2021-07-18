451996bb6d1fdaf1fd0c342493ecddd6610e3df4
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-07-17 19:52:35 -0700

    added tables with necessary columns

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS tracker;
+CREATE DATABASE tracker;
+USE tracker;
+
+CREATE TABLE department(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    INDEX dep_ind(department_id),
+    FOREIGN KEY(department_id)
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+);
