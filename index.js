65824f7efa2b36b165440ccd0975b08a65a2fc08
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-07-17 21:44:50 -0700

    added starter prompt for user choices

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 const inquirer = require("inquirer");
+const db = require("./db/index");
+require("console.table");
+
+const start = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+      ],
+    })
+    .then((res) => {
+      switch (res.choice) {
+        case "View all departments":
+          viewAllDepartments();
+          break;
+        case "View all roles":
+          viewAllRoles();
+          break;
+        case "View all employees":
+          viewAllEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employee role":
+          updateEmployee();
+          break;
+        default:
+          console.log("Function broke!");
+          throw Error("Option not available!");
+      }
+    });
+};
