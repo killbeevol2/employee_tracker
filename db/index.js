b6f0642a81cd2c9bfc0347bd22f5332458db1682
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-07-18 18:00:28 -0700

    made function for viewAllEmployees

--- db/index.js
+++ db/index.js
@@ -12,6 +12,11 @@ class DB {
       "SELECT role.id, role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id"
     );
   }
+  viewAllEmployees() {
+    return this.connection.query(
+      'SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, concat(manager.first_name," ",manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;'
+    );
+  }
 }
 
 module.exports = new DB(connection);
